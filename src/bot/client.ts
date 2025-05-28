509ad87a6b880daf132cbac693d8e23dce1440e5
Sarim <sarim.xs@gmail.com>
2025-05-28 03:02:32 -0600

    Wording

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -123,7 +123,7 @@ export class Bot {
                 }
             )
             console.log(
-                `Private Bot Commands Synced: ${slashCommands
+                `Commands Synced: ${slashCommands
                     .map((c) => `/${c.name}`)
                     .join(', ')}`
             )
